35f7324b87ecd43852e6a41f3fd5597ae15bf687
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-02-25 21:28:25 +0900

    Figuring out HTTP server for node.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+const http = require('node:http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(req.url);
+    res.end();
+  }).listen(8080);
