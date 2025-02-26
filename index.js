9b247116b110bed8a6faedb0d970cd733fd492fe
Kyle W <125979751+MisterShika@users.noreply.github.com>
2025-02-26 13:36:00 +0900

    Node now redirecting properly.

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const http = require('node:http');
+const url = require('url');
+const fs = require('fs');
+
+const display404 = (res) => {
+  const location404 = './404.html';
+  fs.readFile(location404, function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+};
+
+const redirectHTML = (filename, res) => {
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      return display404(res);
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+};
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(req.url);
-    res.end();
-  }).listen(8080);
+  const q = url.parse(req.url, true);
+  let filename = '.' + q.pathname;
+
+  if (filename === './'){
+    filename = './index.html';
+  }
+
+  redirectHTML(filename, res);
+}).listen(8080, () => {
+  console.log('Link : http://localhost:8080/');
+});
